01ecfc0684b796c3ee0e8d1c4b9f302890a4f9dd
ABBY <abbyW@moringa.com>
2022-04-10 17:03:00 +0300

    created a quote constructor in quote class.

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,2 +1,6 @@
 export class Quote {
+    public showDescription: boolean;
+    constructor(public text: string,public author: string, public originDate: Date){
+      this.showDescription=false;
+    }
 }
