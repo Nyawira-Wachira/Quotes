74b4808119f3b70c5c1f30d4a609c57b254c51cb
ABBY <abbyW@moringa.com>
2022-04-11 12:05:41 +0300

    Interpolation of quote author in quotedetail template.

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -12,6 +12,7 @@ export class QuoteComponent implements OnInit {
     new Quote(2,'Yesterday is history, tomorrow is a mystery, today is a gift...that is why it is called present.',' Master Oogway-Kungfu Panda'),
     new Quote(3,'The mark of a true hero is humility.','Master Shifu')
   ];
+ 
   constructor() { }
 
   ngOnInit(): void {
