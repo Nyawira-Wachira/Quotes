43dbf8960bd21a985241787923ed718927137c08
ABBY <abbyW@moringa.com>
2022-04-12 02:16:32 +0300

    Created a submit button on the form.

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,6 @@
 export class Quote {
     public showDescription: boolean;
-    constructor(public id: number ,public text: string, public author: string, public creationDate: Date){
+    constructor(public id: number ,public text: string, public author: string, public creationDate: Date, public name: string){
       this.showDescription=false;
     }
 }
