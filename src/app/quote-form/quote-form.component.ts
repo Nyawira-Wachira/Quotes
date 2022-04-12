e2a74339972863a0b407a95cdd8bc9002209777f
ABBY <abbyW@moringa.com>
2022-04-12 12:41:47 +0300

    Added submit functionality to the form.

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -8,6 +8,11 @@ import { Quote } from '../quote';
 })
 export class QuoteFormComponent implements OnInit {
   newQuote = new Quote(0,"","","", new Date());
+  @Output() addQuote = new EventEmitter<Quote>();
+
+  submitQuote(){
+this.addQuote.emit(this.newQuote);
+  }
 
   constructor() { }
 
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,3 +1,4 @@
+import { DatePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { Quote } from '../quote';
 
@@ -12,6 +13,14 @@ export class QuoteComponent implements OnInit {
     new Quote(2,'Yesterday is history, tomorrow is a mystery, today is a gift...that is why it is called present.', ' ~Master Oogway~', 'Liam Jerry', new Date(2022, 3, 10) ),
     new Quote(3,'The mark of a true hero is humility.','~Master Shifu~', 'Kylie James', new Date(2022, 3, 15))
   ];
+  addNewQuote(quote){
+    let quoteLength = this.quotes.length;
+    quote.id = quoteLength+1;
+    quote.author = quote.author;
+    quote.name = quote.name;
+    quote.creationDate= new Date(quote.creationDate)
+    this.quotes.push(quote)
+  }
   toggleDetails(index){
     this.quotes[index].showDescription = !this.quotes[index].showDescription;
   }
